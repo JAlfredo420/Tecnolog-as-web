5d2b00e565dd6d5a013d647e1c4a8942db4fa3d6
JAlfredo420 <323030045@upjr.edu.mx>
2025-05-20 08:08:16 -0600

    Update p1.js

--- Proyectos/p1.js
+++ Proyectos/p1.js
@@ -1,7 +1,8 @@
 (() => {
-    const addTwoNumbers = (a, b) => {
-        return a + b;
-    };
-    console.log('Resultado 1:', addTwoNumbers(2, 4)); // Esperado: 6
-    console.log('Resultado 2:', addTwoNumbers(10, 10)); // Esperado: 20
+  const addTwoNumbers = (a: number, b: number): number => {
+    return a + b;
+  };
+
+  console.log('Resultado 1:', addTwoNumbers(2, 4));   // Esperado: 6
+  console.log('Resultado 2:', addTwoNumbers(10, 10)); // Esperado: 20
 })();
