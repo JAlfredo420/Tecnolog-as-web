1092602522d31999cfbfa495febec3ca089b2e8e
JAlfredo420 <323030045@upjr.edu.mx>
2025-05-20 08:05:56 -0600

    Add files via upload

---
+++ Proyectos/p1.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    const addTwoNumbers = (a, b) => {
+        return a + b;
+    };
+    console.log('Resultado 1:', addTwoNumbers(2, 4)); // Esperado: 6
+    console.log('Resultado 2:', addTwoNumbers(10, 10)); // Esperado: 20
+})();
